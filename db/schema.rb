d0f8ec45877910144d3d195169f26a5a3429fc78
Elizabeth <mwendemuthusi6@gmail.com>
2023-04-15 19:35:10 +0300

    Edited setup and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_052341) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_163104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "movies", force: :cascade do |t|
+  create_table "songs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
